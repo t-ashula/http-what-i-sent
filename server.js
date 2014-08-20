d2e9e70bfd1e9cf17cf84537935f69bd13748127
t-ashula <office@ashula.info>
2014-08-20 19:26:41 +0900

    add cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 
 'use strict';
 var express = require('express'),
-    helmet = require('helmet');
-var app = express();
+    helmet  = require('helmet'),
+    cors    = require('cors');
+var app     = express();
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost',
     port    = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || 9000;
+app.use(cors());
 app.use(helmet());
 app.use(function(req, res, next) {
     var r = {
