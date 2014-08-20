52438bfd2115ae26d22a95e7aeb321bda4f9f09d
t-ashula <office@ashula.info>
2014-08-20 18:12:57 +0900

    add req.headers to response

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ var app = express();
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost',
     port    = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || 9000;
 app.use(function(req, res, next) {
-    console.log('%s %s %s', req.method, req.url, req.path);
-    res.send({ 
+    var r = {
         method: req.method,
         url : req.url,
-        path: req.path
-    });
+        path: req.path,
+        query: req.query,
+        headers: req.headers
+    };
+    console.log(r);
+    res.send(r);
 });
 
 app.listen(port, ipaddr, function(){
