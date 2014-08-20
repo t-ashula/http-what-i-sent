c88af082dda2c3f8c44b2389b9602a29f70e8952
t-ashula <office@ashula.info>
2014-08-20 18:51:45 +0900

    add helmet

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // server.js
 
 'use strict';
-var express = require('express');
+var express = require('express'),
+    helmet = require('helmet');
 var app = express();
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost',
     port    = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || 9000;
+app.use(helmet());
 app.use(function(req, res, next) {
     var r = {
         method: req.method,
